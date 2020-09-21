959a06fbd2ff35d3d89586c773fd316c4fc92ced
Jorge Rodriguez <jorge.rodriguez.r@gmail.com>
2020-09-21 12:10:36 -0500

    Connect DB

--- server/app.js
+++ server/app.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const mysql = require('mysql');
 
 const app = express();
+
+const db = mysql.createConnection({
+    hots: 'localhost',
+    user: 'root',
+    password: 'Jmrl*-963',
+    database: 'rsl_logistic_db'
+});
+
+db.connect((error) => {
+    if(error) {
+        console.log(error);
+    } else {
+        console.log('MSQL connected');
+    }
+});
 
 app.get('/', (req, res) => {
     res.send('<h1>Home page</h1>');
