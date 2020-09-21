5680c85e77ffc0c786d86f5a63216ce735048c71
Jorge Rodriguez <jorge.rodriguez.r@gmail.com>
2020-09-21 17:29:45 -0500

    Insert register data in db

--- server/app.js
+++ server/app.js
@@ -1,25 +1,45 @@
 const express = require('express');
 const mysql = require('mysql');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config({path: './.env'});
 
 const app = express();
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cors());
+
 const db = mysql.createConnection({
-    hots: 'localhost',
-    user: 'root',
-    password: 'Jmrl*-963',
-    database: 'rsl_logistic_db'
+    hots: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
 });
 
 db.connect((error) => {
     if(error) {
         console.log(error);
     } else {
-        console.log('MSQL connected');
+        console.log('MYSQL connected');
     }
 });
 
-app.get('/', (req, res) => {
-    res.send('<h1>Home page</h1>');
+app.post('/register', (req, res) => {
+
+    const name = req.body.Nombre;
+    const lastName = req.body.Apellido;
+    const email = req.body.Email;
+    const phone = req.body.Telefono;
+    const password = req.body.Contrasena;
+
+    db.query('INSERT INTO usuario (Nombre, Apellido, Email, Telefono, Contrasena) VALUES (?,?,?,?,?)',
+    [name, lastName, email, phone, password],
+    (err, result) => {
+        console.log(err);
+    });
+
 });
 
 app.listen(5000, () => {
